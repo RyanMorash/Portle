38d261e25fe86cf11707e875311af000285f7099
Ryan Morash <ryan@morash.me>
2025-08-05 00:11:57 +0000

    Revert "Use UTC time for date handling in getWordOfTheDay function for consistency"
    This reverts commit 542fa6845574cfc73c59c22c49f0217119b949b2.

--- script.js
+++ script.js
@@ -485,7 +485,7 @@ function getWordOfTheDay(seed) {
 	if (seed === undefined) {
 		let curDate = new Date();
 		// Use UTC time for consistency
-		let utcDate = new Date(Date.UTC(curDate.getUTCFullYear(), curDate.getUTCMonth(), curDate.getUTCDate()));
+		let utcDate = new Date(curDate.getUTCFullYear(), curDate.getUTCMonth(), curDate.getUTCDate());
 		let func = mulberry32(utcDate.getTime());
 		let num = Math.floor(func() * wordList.length);
 		
